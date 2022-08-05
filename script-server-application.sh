23d784c132d7efd644540a9500b15901ff8b34a4
Thiago Tim <thiagotimdev@gmail.com>
2022-08-05 12:46:28 -0300

    Update script-server-application.sh

--- script-server-application.sh
+++ script-server-application.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-echo "Atualizando o servidor..."
+echo "Updating the server..."
 
 apt-get update
 apt-get upgrade -y
 apt-get install apache2 unzip -y
 
-echo "Baixando e copiando os arquivos da aplicação..."
+echo "Downloading application files..."
 
 cd /tmp
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip 
 unzip main.zip
 
 cd linux-site-dio-main
-cd -R * /var/www/html
+cp -R * /var/www/html
 
